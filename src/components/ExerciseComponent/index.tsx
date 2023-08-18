0e8488d818b5c14f0d3e8d4cc423ca708a7f33b7
Samuel D Siqueira <samueldsiqueira@gmail.com>
2023-08-18 19:03:51 -0300

    nex step on style

--- src/components/ExerciseComponent/index.tsx
+++ src/components/ExerciseComponent/index.tsx
@@ -1,76 +1,120 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 interface ExerciseProgram {
-  name: string;
-  steps: string[];
+	name: string;
+	steps: string[];
 }
 
 interface ExerciseProps {
-  programs: {
-    pushups: ExerciseProgram[];
-    situps: ExerciseProgram[];
-  };
+	programs: {
+		pushups: ExerciseProgram[];
+		situps: ExerciseProgram[];
+	};
 }
 
 const ExerciseComponent: React.FC<ExerciseProps> = ({ programs }) => {
-  const [currentProgram, setCurrentProgram] = useState('pushups');
-  const [currentSession, setCurrentSession] = useState(0);
-  const [steps, setSteps] = useState<string[]>([]);
-  const [currentStep, setCurrentStep] = useState(0);
-  const [clockValue, setClockValue] = useState(0);
+	const [currentProgram, setCurrentProgram] = useState('pushups');
+	const [currentSession, setCurrentSession] = useState(0);
+	const [steps, setSteps] = useState<string[]>([]);
+	const [stepStage, setStepStage] = useState<string[]>([]);
+	const [currentStep, setCurrentStep] = useState(0);
 
-  const setProgram = (program: string) => {
-    setCurrentProgram(program);
-    setCurrentSession(0); // Reset session when changing program
-  };
+	const setProgram = (program: string) => {
+		setCurrentProgram(program);
+		setCurrentSession(0); // Reset session when changing program
+	};
 
-const setSession = useCallback((session: number) => {
-  setCurrentSession(session);
-  const program = programs[currentProgram as keyof typeof programs]?.[session];
-  if (program) {
-    setSteps(program.steps);
-    setCurrentStep(0);
-  }
-}, [currentProgram, programs]);
-  useEffect(() => {
-    setSession(currentSession);
-  }, [currentProgram, currentSession, setSession]);
+	const setSession = useCallback(
+		(session: number) => {
+			setCurrentSession(session);
+			const program = programs[currentProgram as keyof typeof programs]?.[session];
+			if (program) {
+				setSteps(program.steps);
+				setCurrentStep(0);
+				setStepStage(program.steps.map(() => 'transparent'));
+			}
+		},
+		[currentProgram, programs],
+	);
 
-  const handleGoButton = () => {
-    if (currentStep < steps.length - 1) {
-      setCurrentStep(currentStep + 1);
-    } else {
-      setSession(currentSession + 1);
-    }
-    setClockValue(0);
-  };
+	useEffect(() => {
+		setSession(currentSession);
+	}, [currentSession, setSession]);
 
-  return (
-    <div className='flex items-center justify-center bg-gray-800 opacity-95 h-screen w-screen'>
-<div className='flex justify-center border-t-gray-800 w-4/6 h-5/6  rounded-lg bg-gray-600 bg-opacity-90 items-center content-center'>
-  <div className='py-4  '>
-    <div>
-      <button className='h-10 w-10 rounded-full' onClick={() => setProgram('pushups')}>Pushups</button>
-    </div>
-    <div>
-      <button className='h-10 w-10 rounded-full' onClick={() => setProgram('situps')}>Situps</button>
-    </div>
-  </div>
-      <div className=' flex-col items-center content-center '>
-    <h2>Current Program: {currentProgram}</h2>
-    <h3>Current Session: {currentSession}</h3>
-    {steps.map((step, index) => (
-      <div key={index} className={`py-2 ${index <= currentStep ? 'text-green-500' : ''}`}>
-        <h4>Step {index + 1}</h4>
-        <p>{step}</p>
-      </div>
-    ))}
-    <button onClick={handleGoButton}>Next Step</button>
-  </div>
-</div>
-    </div>
+	const handleGoButton = () => {
+		if (currentStep < steps.length - 1) {
+			const updatedStages = [...stepStage];
+			if (updatedStages[currentStep] === 'transparent') {
+				updatedStages[currentStep] = 'pink';
+			}
+			setStepStage(updatedStages);
+			setCurrentStep(currentStep + 1);
+		} else {
+			setSession(currentSession + 1);
+		}
+	};
 
-  );
+	return (
+		<div className='container bg-transparent flex items-center justify-center'>
+			<div className='container w-5/6 h-5/6 flex flex-col items-center justify-center rounded-3xl'>
+				<div className='container flex justify-center gap-20 py-1'>
+					<button
+						className={`w-20 h-auto text-zinc-50 outline outline-offset-1 border border-transparent rounded-full hover:border-black ${
+							currentProgram === 'pushups'
+								? 'bg-slate-600 text-neutral-50'
+								: 'bg-slate-800 hover:bg-slate-600 hover:text-neutral-50'
+						}`}
+						onClick={() => setProgram('pushups')}
+					>
+						Pushups
+					</button>
+					<button
+						className={`w-20 h-auto text-zinc-50 outline outline-offset-1 border border-transparent rounded-full hover:border-black ${
+							currentProgram === 'situps'
+								? 'bg-slate-600 text-neutral-50'
+								: 'bg-slate-800 hover:bg-slate-600 hover:text-neutral-50'
+						}`}
+						onClick={() => setProgram('situps')}
+					>
+						Situps
+					</button>
+				</div>
+				<div className='w-10/12 rounded-full flex flex-col justify-center items-center content-center'>
+					<h2 className='text-zinc-50'>Current Program: {currentProgram}</h2>
+					<h3 className='text-zinc-50'>Current Session: {currentSession}</h3>
+					<div className='flex flex-col gap-2 items-center'>
+						{steps.map((step, index) => (
+							<div
+								key={index}
+								className={`${
+									stepStage[index] === 'pink'
+										? 'flex flex-col items-center text-zinc-50 bg-gray-300 rounded-full w-52'
+										: index === currentStep
+										? 'flex flex-col items-center text-green-500 bg-pink-500 rounded-full w-52'
+										: 'flex flex-col items-center text-zinc-50 bg-transparent w-auto'
+								}`}
+							>
+								<div>
+									<h4 className='text-zinc-50 text'>Step {index + 1}</h4>
+								</div>
+								<div>
+									<p>{step}</p>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className='mx-auto py-2'>
+						<button
+							className='w-20 h-auto text-zinc-50 outline outline-offset-1 border border-transparent rounded-full hover:border-black hover:bg-slate-600 hover:text-neutral-50'
+							onClick={handleGoButton}
+						>
+							Next Step
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ExerciseComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,78 +2,78 @@ import React from 'react';
 import ExerciseComponent from '../components/ExerciseComponent';
 
 function Index(): React.JSX.Element {
-    const programs = {
-    pushups: [
-		{
-			name: 'w3t1d1',
-			steps: [
-				'do 15 pushups',
-				'rest 60 seconds',
-				'do 18 pushups',
-				'rest 60 seconds',
-				'do 11 pushups',
-				'rest 60 seconds',
-				'do 11 pushups',
-				'rest 60 seconds',
-				'do at least 14 pushups',
-				'done!',
-			],
-		},
-		{
-			name: 'w3t1d2',
-			steps: [
-				'do 15 pushups',
-				'rest 60 seconds',
-				'do 18 pushups',
-				'rest 60 seconds',
-				'do 12 pushups',
-				'rest 60 seconds',
-				'do 12 pushups',
-				'rest 60 seconds',
-				'do at least 18 pushups',
-				'done!',
-			],
-		},
-	],
+	const programs = {
+		pushups: [
+			{
+				name: 'w3t1d1',
+				steps: [
+					'do 15 pushups',
+					'rest 60 seconds',
+					'do 18 pushups',
+					'rest 60 seconds',
+					'do 11 pushups',
+					'rest 60 seconds',
+					'do 11 pushups',
+					'rest 60 seconds',
+					'do at least 14 pushups',
+					'done!',
+				],
+			},
+			{
+				name: 'w3t1d2',
+				steps: [
+					'do 15 pushups',
+					'rest 60 seconds',
+					'do 18 pushups',
+					'rest 60 seconds',
+					'do 12 pushups',
+					'rest 60 seconds',
+					'do 12 pushups',
+					'rest 60 seconds',
+					'do at least 18 pushups',
+					'done!',
+				],
+			},
+		],
 
-	situps: [
-		{
-			name: 'w3t1d1',
-			steps: [
-				'do 15 situps',
-				'rest 60 seconds',
-				'do 18 situps',
-				'rest 60 seconds',
-				'do 11 situps',
-				'rest 60 seconds',
-				'do 11 situps',
-				'rest 60 seconds',
-				'do at least 14 situps',
-				'done!',
-			],
-		},
-		{
-			name: 'w3t1d2',
-			steps: [
-				'do 15 situps',
-				'rest 60 seconds',
-				'do 18 situps',
-				'rest 60 seconds',
-				'do 12 situps',
-				'rest 60 seconds',
-				'do 12 situps',
-				'rest 60 seconds',
-				'do at least 18 situps',
-				'done!',
-			],
-		},
-	],
-  };
-  return (
-    <div className=' h-screen w-screen'>
-      <ExerciseComponent programs={programs} />
-    </div>
-  );
+		situps: [
+			{
+				name: 'w3t1d1',
+				steps: [
+					'do 15 situps',
+					'rest 60 seconds',
+					'do 18 situps',
+					'rest 60 seconds',
+					'do 11 situps',
+					'rest 60 seconds',
+					'do 11 situps',
+					'rest 60 seconds',
+					'do at least 14 situps',
+					'done!',
+				],
+			},
+			{
+				name: 'w3t1d2',
+				steps: [
+					'do 15 situps',
+					'rest 60 seconds',
+					'do 18 situps',
+					'rest 60 seconds',
+					'do 12 situps',
+					'rest 60 seconds',
+					'do 12 situps',
+					'rest 60 seconds',
+					'do at least 18 situps',
+					'done!',
+				],
+			},
+		],
+	};
+	return (
+		<div className='flex mx-auto backdrop-blur-lg bg-black/30 h-screen w-screen'>
+			<ExerciseComponent programs={programs} />
+		</div>
+	);
 }
 
 export default Index;
