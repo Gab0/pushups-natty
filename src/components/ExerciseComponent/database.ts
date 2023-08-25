49013459f6d25f30164edef526f14dd571df7cd4
Gabriel Araujo <gabriel_scf@hotmail.com>
2023-08-25 17:43:02 -0300

    Implement proper exercise database parsing routines.

--- src/components/ExerciseComponent/database.ts
+++ src/components/ExerciseComponent/database.ts
@@ -1,24 +1,52 @@
 // database.ts
 //
+// Load the database and process its data so it can be used
+// by all the other modules;
 import exerciseDatabase from "../../pages/api/routines.json";
 
 interface ProgramSeed {
 	name: string;
-	steps: number[];
+	steps?: string[];
+	type: string;
+	twd?: number[][][][];
 }
 
-interface ParsedProgram {
+interface ExerciseDay {
 	name: string;
 	steps: string[];
 }
 
-const parseProgram = (programSeed: ProgramSeed): ParsedProgram => {
+interface ExerciseProgram {
+	name: string;
+	days: ExerciseDay[];
+}
+
+const parseTWD = (value_array: number[][][][]): ExerciseDay[] => {
+
+	let programs = [];
+	for (let t = 0; t < value_array.length; t++) {
+		for (let w = 0; w < value_array[t].length; w++) {
+			for (let d = 0; d < value_array[t][w].length; d++) {
+				programs.push({
+					name: `Tier ${t + 1} Week ${w + 1} Day ${d + 1}`,
+					steps: parseProgramNumbers(value_array[t][w][d])
+				})
+			}
+		}
+	}
+
+	return programs;
+}
+
+
+const parseProgramNumbers = (repetitionSeries: number[]): string[] => {
+
 	const content: string[] = [];
 	let last: number | boolean = 0;
 	let message = '';
 
-	for (let a = 0; a < programSeed.steps.length; a++) {
-		last = a === programSeed.steps.length - 1;
+	for (let a = 0; a < repetitionSeries.length; a++) {
+		last = a === repetitionSeries.length - 1;
 
 		if (last) {
 			message = 'do at least ';
@@ -26,7 +54,7 @@ const parseProgram = (programSeed: ProgramSeed): ParsedProgram => {
 			message = 'do ';
 		}
 
-		content.push(message + programSeed.steps[a] + ' pushups');
+		content.push(message + repetitionSeries[a] + ' pushups');
 
 		if (!last) {
 			content.push('rest 60 seconds');
@@ -35,10 +63,26 @@ const parseProgram = (programSeed: ProgramSeed): ParsedProgram => {
 
 	content.push('done!');
 
+	return content;
+
+}
+
+const parseProgram = (programSeed: ProgramSeed): ExerciseProgram => {
+
+	let steps: ExerciseDay[] = [];
+
+	if (programSeed.twd !== undefined) {
+		steps = parseTWD(programSeed.twd);
+	}
+
+	if (programSeed.steps !== undefined) {
+		steps = [{name: "Singleton", steps: programSeed.steps}];
+	}
+
 	return {
 		name: programSeed.name,
-		steps: content,
+		days: steps,
 	};
 };
 
-export const pushupContent: ParsedProgram[] = programSeeds.map(parseProgram);
+export const pushupContent: ExerciseProgram[] = exerciseDatabase.map(parseProgram);
